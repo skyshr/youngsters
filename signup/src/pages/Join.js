1472ade5b5076015c5850b6827bd85279e1611d4
Hyein <goldenoriole34@gmail.com>
2021-12-29 00:32:19 +0900

    join

--- signup/src/pages/Join.js
+++ signup/src/pages/Join.js
@@ -4,26 +4,40 @@ export default function Join(){
   return(
     <>
       <div className='wrap'>
-        <div className='join-container'>
+        <form className='join-container'>
           <div className='join-top'>
             <h1>JOIN</h1>
           </div>
           <div className='join-middle'>
-              <form className='middle'>
-                <input placeholder='사용할 아이디'></input>
-                <input placeholder='비밀번호'></input>
-                <input placeholder='이름'></input>
-                <input placeholder='성별'></input>
-                <input placeholder='나이'></input>
-                <input placeholder='우편번호'></input>
-                <input placeholder='주소'></input>
-                <input placeholder='상세주소'></input>
-              </form>
+              <div className='middle'>
+                <div>
+                  <input id='id' type='email' placeholder='사용할 아이디(email 형식)' />
+                </div>
+                <div>
+                  <input id='password' type='password' placeholder='비밀번호' />
+                </div>
+                <div>
+                  <input type='text' placeholder='이름' />
+                </div>
+                <div>
+                  <select id='gender' placeholder='성별'>
+                    <option value='male'>남자</option>
+                    <option value='female'>여자</option>
+                  </select>
+                  <input id='age' type='number' placeholder='나이' />
+                </div>
+                <div>
+                  <input id='addr1' placeholder='주소' />
+                </div>
+                <div>
+                  <input id='addr2'placeholder='상세주소' />
+                </div>
+              </div>
           </div>
           <div className='join-bottom'>
-          <button>JOIN</button>
+            <button type='submit'>JOIN</button>
           </div>
-        </div>
+        </form>
       </div>
     </>
   )
