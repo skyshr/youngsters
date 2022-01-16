b77915ca9d1f87bf7b4d94e853db2f28f52a3320
Hyein <goldenoriole34@gmail.com>
2022-01-16 22:22:30 +0900

    main

--- project/src/chat/Chat.js
+++ project/src/chat/Chat.js
@@ -135,7 +135,7 @@ function Chat(props) {
         <div className="topBarBox">
           <button id="wrap-btn-gobackhome" onClick={backtoMenu}>
           </button>
-          <div id="partnerName">이혜인 님</div>
+          <div id="partnerName">{} 님</div>
           <div></div>
         </div>
         <div className="ChatBox" id="chatmessages">
